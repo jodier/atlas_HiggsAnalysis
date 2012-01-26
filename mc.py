949b7f044c755da23818584b37867e2578efd8ac
Elodie Tiouchichine <tiouchi@marquark.in2p3.fr>
2012-01-26 11:44:31 +0100

    =new data_egamma.py

--- mc.py
+++ mc.py
@@ -7,11 +7,11 @@ import time, getpass, commands
 #############################################################################
 
 L = [
-	#'mc11_7TeV.107650.AlpgenJimmyZeeNp0_pt20.merge.NTUP_HSG2.e835_s1299_s1300_r2820_r2872_p768/',
-	#'mc11_7TeV.107651.AlpgenJimmyZeeNp1_pt20.merge.NTUP_HSG2.e835_s1299_s1300_r2820_r2872_p768/',
-	#'mc11_7TeV.107652.AlpgenJimmyZeeNp2_pt20.merge.NTUP_HSG2.e835_s1299_s1300_r2820_r2872_p768/',
-	#'mc11_7TeV.107653.AlpgenJimmyZeeNp3_pt20.merge.NTUP_HSG2.e835_s1299_s1300_r2820_r2872_p768/',
-	#'mc11_7TeV.107654.AlpgenJimmyZeeNp4_pt20.merge.NTUP_HSG2.e835_s1299_s1300_r2820_r2872_p768/',
+	'mc11_7TeV.107650.AlpgenJimmyZeeNp0_pt20.merge.NTUP_HSG2.e835_s1299_s1300_r2820_r2872_p768/',
+	'mc11_7TeV.107651.AlpgenJimmyZeeNp1_pt20.merge.NTUP_HSG2.e835_s1299_s1300_r2820_r2872_p768/',
+	'mc11_7TeV.107652.AlpgenJimmyZeeNp2_pt20.merge.NTUP_HSG2.e835_s1299_s1300_r2820_r2872_p768/',
+	'mc11_7TeV.107653.AlpgenJimmyZeeNp3_pt20.merge.NTUP_HSG2.e835_s1299_s1300_r2820_r2872_p768/',
+	'mc11_7TeV.107654.AlpgenJimmyZeeNp4_pt20.merge.NTUP_HSG2.e835_s1299_s1300_r2820_r2872_p768/',
 	'mc11_7TeV.107655.AlpgenJimmyZeeNp5_pt20.merge.NTUP_HSG2.e835_s1299_s1300_r2820_r2872_p768/',
 	#'mc11_7TeV.107660.AlpgenJimmyZmumuNp0_pt20.merge.NTUP_HSG2.e835_s1299_s1300_r2820_r2872_p768/',
 	#'mc11_7TeV.107661.AlpgenJimmyZmumuNp1_pt20.merge.NTUP_HSG2.e835_s1299_s1300_r2820_r2872_p768/',
